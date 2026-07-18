1351e96bb37fca66594ff3f13f929444d72046d5
nickshaeric <nicksha@tuta.io>
2026-07-18 16:02:57 -0600

    feat(pkgs): add cli's for doomemacs

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,8 +1,12 @@
 { pkgs, ... }: {
   environment.systemPackages = [
+    pkgs.coreutils
     pkgs.curl
     pkgs.git
     pkgs.neovim
+    pkgs.pandoc
+    pkgs.ripgrep
+    pkgs.shellcheck
     pkgs.stow
     pkgs.tmux
     pkgs.tree
